63474d6a8b742b5c5545cc35fb250df7e9adca79
Brian174tech <114503613+Brian174tech@users.noreply.github.com>
2026-04-18 00:15:15 +0200

    Add dropdown to `ControllerComponent` for selecting controller type; enhance `SceneControllerUtil` with prefab replacement and cleanup logic.

--- Editor/Components/ControllerComponent.cs
+++ Editor/Components/ControllerComponent.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -8,15 +9,40 @@ namespace Rotterdam.DigitalTwins.Editor
         public ControllerComponent()
         {
             style.flexGrow = 1;
+            
             Label label = new Label("Experience & Controller");
             label.style.fontSize = 16;
             label.style.unityFontStyleAndWeight = FontStyle.Bold;
             Add(label);
 
-            Label info = new Label("Kies uit VR, AR of Desktop prefabs.");
+            Label info = new Label("Choose a controller type to use in the scene.");
             info.style.whiteSpace = WhiteSpace.Normal;
             info.style.marginTop = 10;
+            info.style.marginBottom = 15;
             Add(info);
+
+            List<string> options = new List<string> { "None", "First Person", "Third Person" };
+            string currentType = SceneControllerUtil.GetCurrentControllerType();
+            
+            DropdownField dropdown = new DropdownField("Controller Type", options, currentType);
+            dropdown.RegisterValueChangedCallback(evt =>
+            {
+                switch (evt.newValue)
+                {
+                    case "First Person":
+                        SceneControllerUtil.ReplaceController("FirstPerson-Rig");
+                        break;
+                    case "Third Person":
+                        SceneControllerUtil.ReplaceController("ThirdPerson-Rig");
+                        break;
+                    case "None":
+                    default:
+                        SceneControllerUtil.RemoveExistingControllers();
+                        break;
+                }
+            });
+
+            Add(dropdown);
         }
     }
 }
--- Editor/Utilities/SceneControllerUtil.cs
+++ Editor/Utilities/SceneControllerUtil.cs
@@ -1,7 +1,61 @@
+using UnityEditor;
+using UnityEngine;
+using Rotterdam.DigitalTwins.Runtime;
+
 namespace Rotterdam.DigitalTwins.Editor
 {
-    public class SceneControllerUtil
+    public static class SceneControllerUtil
     {
-        
+        private const string PackagePath = "Packages/com.rotterdam.digital-twins/Runtime/Prefabs/Controllers/";
+        private const string LocalPath = "Assets/Runtime/Prefabs/Controllers/";
+
+        public static void ReplaceController(string prefabName)
+        {
+            RemoveExistingControllers();
+
+            string fullPath = $"{PackagePath}{prefabName}.prefab";
+            GameObject prefab = AssetDatabase.LoadAssetAtPath<GameObject>(fullPath);
+            if (prefab == null)
+            {
+                fullPath = $"{LocalPath}{prefabName}.prefab";
+                prefab = AssetDatabase.LoadAssetAtPath<GameObject>(fullPath);
+            }
+
+            if (prefab == null)
+            {
+                Debug.LogError($"Controller prefab {prefabName} niet gevonden op paden: {PackagePath} of {LocalPath}");
+                return;
+            }
+
+            GameObject instance = (GameObject)PrefabUtility.InstantiatePrefab(prefab);
+            Undo.RegisterCreatedObjectUndo(instance, "Instantiate Controller");
+            Selection.activeGameObject = instance;
+            
+            Debug.Log($"Controller vervangen door {prefabName}.");
+        }
+
+        public static void RemoveExistingControllers()
+        {
+            var firstPersonControllers = Object.FindObjectsByType<FirstPersonController>(FindObjectsInactive.Include, FindObjectsSortMode.None);
+            foreach (var controller in firstPersonControllers)
+            {
+                Undo.DestroyObjectImmediate(controller.gameObject);
+            }
+
+            var thirdPersonControllers = Object.FindObjectsByType<ThirdPersonController>(FindObjectsInactive.Include, FindObjectsSortMode.None);
+            foreach (var controller in thirdPersonControllers)
+            {
+                Undo.DestroyObjectImmediate(controller.gameObject);
+            }
+        }
+
+        public static string GetCurrentControllerType()
+        {
+            if (Object.FindAnyObjectByType<FirstPersonController>(FindObjectsInactive.Include) != null)
+                return "First Person";
+            if (Object.FindAnyObjectByType<ThirdPersonController>(FindObjectsInactive.Include) != null)
+                return "Third Person";
+            return "None";
+        }
     }
 }
