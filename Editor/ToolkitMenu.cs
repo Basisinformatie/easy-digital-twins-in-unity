e47e50654f4e125e4911792c8cdd569ae71025aa
Brian174tech <114503613+Brian174tech@users.noreply.github.com>
2026-04-20 22:53:10 +0200

    Revert "Remove Cesium dependencies and refactor georeferencing logic: extract and conditionalize Cesium features using `CESIUM_PRESENT`, update assembly definitions, and improve 3D Tileset setup robustness."
    This reverts commit 89925790be530a3ba94049919568288dffdd2f6b.

---
+++ Editor/ToolkitMenu.cs
@@ -0,0 +1,15 @@
+using UnityEditor;
+using Rotterdam.DigitalTwins.Editor;
+
+namespace Rotterdam.DigitalTwins.Editor
+{
+    public class ToolkitMenu
+    {
+        [MenuItem("Rotterdam Digital Twins/Launch UI")]
+        public static void OpenShoppingWindow()
+        {
+            CesiumSetupService.EnsureCesiumIsInstalled();
+            ShoppingWindow.ShowWindow();
+        }
+    }
+}
