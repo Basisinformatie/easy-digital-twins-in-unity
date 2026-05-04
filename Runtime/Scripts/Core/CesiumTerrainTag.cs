ffab68e3a19dff7c10fc170ea29c78e85f457c90
Brian174tech <114503613+Brian174tech@users.noreply.github.com>
2026-05-04 14:36:54 +0200

    added terraintag

---
+++ Runtime/Scripts/Core/CesiumTerrainTag.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace Rotterdam.DigitalTwins.Runtime
+{
+    /// <summary>
+    /// A simple tag component to identify Cesium 3D Terrain tilesets in the scene.
+    /// This allows the toolkit to automatically find terrain for WMS overlays.
+    /// </summary>
+    [AddComponentMenu("Rotterdam Digital Twin/Cesium Terrain Tag")]
+    public class CesiumTerrainTag : MonoBehaviour
+    {
+    }
+}
