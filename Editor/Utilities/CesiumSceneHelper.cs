e6d350ccbbe3feaac7b1e80794e3b6745ecfe130
Brian174tech <114503613+Brian174tech@users.noreply.github.com>
2026-05-04 15:10:10 +0200

    Revert "Set `wms.layers` default value to `0` in `CesiumSceneHelper`."
    This reverts commit ca8d56495b92fb81b6403eacecd4a519b48eccd6.

--- Editor/Utilities/CesiumSceneHelper.cs
+++ Editor/Utilities/CesiumSceneHelper.cs
@@ -219,7 +219,7 @@ namespace Rotterdam.DigitalTwins.Editor
 
             CesiumWebMapServiceRasterOverlay wms = target.AddComponent<CesiumWebMapServiceRasterOverlay>();
             wms.baseUrl = baseUrl;
-            wms.layers = 0;
+            wms.layers = layers;
             wms.maximumLevel = 22;
 
             Undo.RegisterCreatedObjectUndo(wms, "Add WMS Overlay");
