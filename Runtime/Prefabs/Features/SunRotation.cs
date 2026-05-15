fae146caed3dfc6488af357c24c0756a05530ed5
Brian174tech <114503613+Brian174tech@users.noreply.github.com>
2026-05-15 16:29:03 +0200

    Add `Adaptive Lighting` prefab, `ToolboxSky1` material, and update `SunRotation` script with new rotation modes

--- Runtime/Prefabs/Features/SunRotation.cs
+++ Runtime/Prefabs/Features/SunRotation.cs
@@ -3,11 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 public class SunRotation : MonoBehaviour
 {
+    public enum RotationMode { Continuous, SpecificTime }
+    [Tooltip("Pick between continuous or  a specific time.")]
+    public RotationMode mode;
+
+    [Tooltip("Continuous mode: degrees per frame. Y and Z determine orientation and orbit.")]
     public Vector3 rotationSet;
+    
+    [Range(0, 24)]
+    [Tooltip("Time of day (0-24hours). Only used in 'SpecificTime' mode.")]
+    public float timeOfDay = 19.3f;
     
     private void Update()
     {
-        transform.Rotate(rotationSet, Space.World);
+        if (mode == RotationMode.Continuous)
+        {
+            transform.Rotate(rotationSet, Space.World);
+        }
+        else
+        {
+            UpdateSunPosition();
+        }
     }
-    
+
+    private void OnValidate()
+    {
+        if (mode == RotationMode.SpecificTime)
+        {
+            UpdateSunPosition();
+        }
+    }
+
+    private void UpdateSunPosition()
+    {
+        float angle = ((timeOfDay-1.5f) / 24f) * 360f - 90f;
+        transform.localEulerAngles = new Vector3(angle, rotationSet.y, rotationSet.z);
+    }
 }
